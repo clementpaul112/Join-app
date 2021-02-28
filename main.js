77bc2155eaa69f7e431b112e7279054ad93fb355
clementpaul112 <72744246+clementpaul112@users.noreply.github.com>
2021-02-28 15:25:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+name_of_the_student_array = []; 
+function submit() { var display_student_array = []; 
+for (var j = 1; j <= 4; j++) { var name_of_the_student = document.getElementById("name_of_the_student_"+j).value;
+ console.log(name_of_the_student); 
+ name_of_the_student_array.push(name_of_the_student); }
+ console.log(name_of_the_student_array)
+}
