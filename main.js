5fa496844f8270e76c256de7ceb64e5da92bc676
clementpaul112 <72744246+clementpaul112@users.noreply.github.com>
2021-04-29 12:38:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-name_of_the_student_array = []; 
-function submit() { var display_student_array = []; 
-for (var j = 1; j <= 4; j++) { var name_of_the_student = document.getElementById("name_of_the_student_"+j).value;
- console.log(name_of_the_student); 
- name_of_the_student_array.push(name_of_the_student); }
- console.log(name_of_the_student_array)
+inputs = []; 
+function getPara1() {  
+ for (var i = 1; j <= 6; i++) 
+ {  input.push(document.getElementById("para1_input_box_"+i).value;
+  console.log(input_boxs); 
+  inputs.join(". "); }
+ document.getElementById("showPara1").innerHTML = inputs.join(". ");
+}
+
+function getPara2() { 
+ for (var i = 2; j <= 6; i++) 
+ {  input.push(document.getElementById("para2_input_box_"+i).value;
+  console.log(input_boxs); 
+  inputs.join(". "); }
+ document.getElementById("showPara2").innerHTML = inputs.join(". ");
 }
